938262fed3104c1637d259fabac6904792a368f9
BBZW-Sursee Thuering Lukas <Lukas_Thuering@sluz.ch>
2017-12-04 08:42:29 +0100

    Commented

--- SupportQuerys/Events.cs
+++ SupportQuerys/Events.cs
@@ -1,53 +1,58 @@
-﻿using System;
+﻿using BiCA.Sabas.Extension.V2;
+using OfficeOpenXml;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text;
-using BiCA.Sabas.Extension.V2;
-using OfficeOpenXml;
+using System.Windows.Forms;
 
 namespace SupportQuerys
 {
     class Events : ManagedScript
     {
+        
         protected override void OnInitializing()
         {
-            
-        }
 
+        }
+        
         protected override void OnExecuting()
         {
             #region read eventlog
             //Read eventlog
 
-            StringBuilder sb = new StringBuilder();
             System.Diagnostics.EventLog log = new
             System.Diagnostics.EventLog("Application");
-
-
-
-            DateTime dt = DateTime.MinValue;
+           
+            DateTime dt = new DateTime();
             Console.WriteLine(dt.ToString());
 
             //Create Excel file with the correct name
-            var package = new ExcelPackage(new FileInfo(@"C:\Users\CH-TLU\Desktop\Eventlog" + Runtime.System.Street + "_" + Runtime.System.PostalCode + "_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".xlsx"));
+            //Create file on Server
+            //  var package = new ExcelPackage(new FileInfo(@"\\ch-bicaap02\Support\TEMP\SUPP.evViewer\" + Runtime.System.SisJobNumber + Runtime.System.Street + "_" + Runtime.System.PostalCode + "_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".xlsx"));
+
+            //Create local file (Testing purposes)
+            var package = new ExcelPackage(new FileInfo(@"C:\Temp\" + Runtime.System.SisJobNumber + Runtime.System.Street + "_" + Runtime.System.PostalCode + "_" + DateTime.Now.ToString("yyyyMMdd_hhmmss") + ".xlsx"));
             //Add new worksheet with the name "Events"
             package.Workbook.Worksheets.Add("Events");
             //Go to the first (and only) worksheet in the Excel sheet
             ExcelWorksheet sheet = package.Workbook.Worksheets.First();
 
-            int row = 1;
+            //Row needs to be set to 2, because there will be a filter in row 1
+            int row = 2;
 
             #endregion
-
-
+            
             #region write to excel file & save
+
             //Write eventlog to excel file
 
+            //foreach loop to write the event to the excel
             foreach (System.Diagnostics.EventLogEntry entry in log.Entries)
             {
                 if (entry.TimeGenerated > dt)
                 {
-
+                    sheet.SelectedRange["A1"].Value = "";
                     sheet.Cells[row, 1].Value = Runtime.System.SiteServer.HostAddress;
                     sheet.Cells[row, 2].Value = Runtime.System.SystemID;
                     sheet.Cells[row, 3].Value = entry.Source;
@@ -55,25 +60,30 @@ namespace SupportQuerys
                     sheet.Cells[row, 5].Value = entry.TimeGenerated.Date.ToLongDateString();
                     sheet.Cells[row, 6].Value = entry.TimeWritten.ToLongTimeString();
                     sheet.Cells[row, 7].Value = entry.Message;
-
                     row++;
                 }
             }
-
+            //Sets the Column size to automatic for a better view
             sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
+
+            //saves the file
             package.Save();
-        }
 
             #endregion
+        }
+
+     
+
+        
 
         protected override void OnFinalizing()
         {
-            //Ad here thigns, the program should do on Finalizing
+            
         }
 
         protected override void OnErrorOccurred(Exception exception)
         {
-            
+
         }
     }
 }
